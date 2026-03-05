584c504be30a68aac300539639b37ba2e0d62c57
Franka <fgriem@gmail.com>
2026-03-05 13:02:30 +0100

    js exercise 08

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...toBeRemoved) {
+
+    let newArray = [];
+
+    for (const el of arr){
+        if(toBeRemoved.includes(el)){
+            continue;
+        }
+        newArray.push(el);
+    }
+
+    return newArray;
+
 };
 
 // Do not edit below this line
