1f9a62db720bfb56cb3b1ba4e4f2f028a26950c6
Franka <fgriem@gmail.com>
2026-03-20 12:05:54 +0100

    javascript exercise 12 - calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((result, current)=> result * current);
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+	return base ** exp;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	// fakultaet = produkt aller natuerlichen zahlen bis n: 3! = 3 * 2 * 1
+  let result = 1;
+  for(let i = 1; i <= n; i++){
+    result = result * i;
+  }
+  return result;
 };
 
 // Do not edit below this line
