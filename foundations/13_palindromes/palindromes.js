57d21f5251eb6a2aa0ee2f59e50ddd7bfe1eda1f
Franka <fgriem@gmail.com>
2026-03-20 12:26:50 +0100

    javascript exercise 13 - palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let wordRaw = word.toLowerCase().replace(new RegExp("[^a-z0-9]", "g"), "");
+    
+    let reversed = '';
+    for(let i = wordRaw.length-1; i>=0; i--){
+        reversed += wordRaw[i];
+    }
 
+    return wordRaw === reversed;
 };
 
 // Do not edit below this line
