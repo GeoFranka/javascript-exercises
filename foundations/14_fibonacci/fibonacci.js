ad887d3899ec94462c11c522c838ac4ae078aa35
Franka <fgriem@gmail.com>
2026-03-20 12:50:29 +0100

    javascript exercise 14 - fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,4 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+
+    if(n<0){
+        return "OOPS";
+    } else if(n==0){
+        return 0;
+    }
+
+    let prepreceding = 0;
+    let preceding = 1;
+    for(let i = 2; i<=n; i++){
+        let current = prepreceding + preceding;
+        prepreceding = preceding;
+        preceding = current;
+    }
+
+    return preceding;
 
 };
 
