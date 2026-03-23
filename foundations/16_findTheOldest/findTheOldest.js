8a360d46ad2a53fb94d6061f1c5d9921125ddb5a
Franka <fgriem@gmail.com>
2026-03-23 10:51:12 +0100

    exercise 16, find the oldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,4 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(peoplesArray) {
+
+    let oldest;
+
+    peoplesArray.forEach(person => {
+        
+        person.age = (person.yearOfDeath || new Date().getFullYear()) - person.yearOfBirth;
+
+        if(oldest===undefined || oldest.age < person.age){
+            oldest = person;
+        }
+
+    });
+
+    peoplesArray.forEach(p => {delete p.age});
+
+    return oldest;
 
 };
 
